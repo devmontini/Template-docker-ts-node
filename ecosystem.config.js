c1a4dc11e5c3b44c30792caa0747281f8a037a8d
Montini <devmontini@gmail.com>
2022-10-07 15:26:56 -0300

    Initial commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: '<app-name>',
-      script: 'dist/src/index.js',
+      name: "<app-name>",
+      script: "dist/src/index.js",
       // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-      exec_mode: 'cluster',
+      exec_mode: "cluster",
       instances: 2,
       kill_timeout: 5000,
       autorestart: true,
@@ -13,16 +13,16 @@ module.exports = {
       watch_options: {
         followSymlinks: false,
       },
-      max_memory_restart: '1G',
+      max_memory_restart: "1G",
       env: {
-        NODE_ENV: 'development',
+        NODE_ENV: "development",
       },
       env_production: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
         port: 8080,
       },
       merge_logs: true,
-      log_date_format: 'YYYY-MM-DDTHH:mm:ss.sssZ',
+      log_date_format: "YYYY-MM-DDTHH:mm:ss.sssZ",
     },
   ],
   // https://pm2.keymetrics.io/docs/usage/deployment/
@@ -30,12 +30,12 @@ module.exports = {
     production: {
       key: process.env.EC2_PEM_PATH,
       host: process.env.EC2_HOST,
-      user: 'ubuntu',
-      ref: 'origin/main',
-      repo: 'git@github.com:<username>/<repo-name>.git',
-      path: '/home/ubuntu',
-      'post-deploy':
-        'yarn && yarn clean && yarn build && pm2 startOrRestart ecosystem.config.js --env production && pm2 set pm2:autodump true && pm2 save',
+      user: "ubuntu",
+      ref: "origin/main",
+      repo: "git@github.com:<username>/<repo-name>.git",
+      path: "/home/ubuntu",
+      "post-deploy":
+        "yarn && yarn clean && yarn build && pm2 startOrRestart ecosystem.config.js --env production && pm2 set pm2:autodump true && pm2 save",
     },
   },
 };
--- src/components/home/services.ts
+++ src/components/home/services.ts
@@ -7,16 +7,6 @@ export class HomeServices {
   constructor() {
     this.homeDAO = new HomeDAO();
   }
-  // NAME: pkg.name,
-  // VERSION: pkg.version,
-  // VER: `v${pkg.version[0]}`,
-  // DESCRIPTION: pkg.description,
-  // AUTHORS: pkg.authors,
-  // HOST: process.env.APP_HOST,
-  // BASE_URL: process.env.API_BASE_URL,
-  // PORT: process.env.NODE_ENV === "test" ? 8888 : process.env.PORT || 8080,
-  // ENV: process.env.NODE_ENV,
-
   /**
    * @description Get application information.
    * @returns AppInformation
